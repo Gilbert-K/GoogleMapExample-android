15b6374d8f9ce802d634a2f330d0aa32f1b6f05a
kim yeong jin <86880007+Gilbert-K@users.noreply.github.com>
2022-04-30 14:43:39 +0900

    Add files via upload

---
+++ GoogleMapExample/app/src/main/java/com/example/googlemapexample/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.googlemapexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.485284, 126.901451); //구로디지털단지역 위치
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("구로디지털단지역");
+        markerOptions.snippet("지하철역");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+
+    }
+}
